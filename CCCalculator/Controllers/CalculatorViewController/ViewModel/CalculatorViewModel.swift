30420be4a202ceae72e7f63ce933b8644d852ac2
Amr Hesham <amrhesham9595@gmail.com>
2021-04-28 19:46:01 +0200

    Adding CalculatorViewModel Files
    - Adding BaseViewModel
    - Adding State
    - Adding CalculatorViewModel

---
+++ CCCalculator/Controllers/CalculatorViewController/ViewModel/CalculatorViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorViewModel.swift
+//  CCCalculator
+//
+//  Created by Amr Hesham on 28/04/2021.
+//
+
+import Foundation
+
+// MARK: - CalculatorViewModel
+//
+class CalculatorViewModel: ViewModel {
+  
+}
