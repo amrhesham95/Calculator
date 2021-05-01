e70513d4444aed761ea33b01e62a63c184e1f1e8
Amr Hesham <amrhesham9595@gmail.com>
2021-05-01 18:06:52 +0200

    - Add CurrencyConverterViewControllerTests

--- CCCalculator/CCCalculatorTests/ViewRelated/Controllers/CurrencyConverterViewController/CurrencyConverterViewControllerTests.swift
+++ CCCalculator/CCCalculatorTests/ViewRelated/Controllers/CurrencyConverterViewController/CurrencyConverterViewControllerTests.swift
@@ -21,10 +21,8 @@ class CurrencyConverterViewControllerTests: XCTestCase {
   override func setUp() {
     super.setUp()
     
-//    sut = CurrencyConverterViewController(viewModel: <#T##CurrencyConverterViewModel#>)
-    
-    // load view hierarchy
-//    _ = sut.view
+    sut = CurrencyConverterViewController()
+    sut.loadViewIfNeeded()
   }
   
   override func tearDown() {
@@ -33,6 +31,50 @@ class CurrencyConverterViewControllerTests: XCTestCase {
   }
   
   // MARK: - Tests
+  func testCurrencyConverterViewControllerTest_whenCreated_textFieldKeyboardTypeIsNumber() {
+    
+    // Then
+    XCTAssertEqual(sut.textField.keyboardType, .asciiCapableNumberPad, "keyboard type isn't set correctly")
+  }
+
+  func testCurrencyConverterViewControllerTest_whenCreated_convertButtonIsNotEnabled() {
+    
+    // Then
+    XCTAssertFalse(sut.convertButton.isEnabled, "Convert button should be disabled")
+  }
   
+  func testCurrencyConverterViewControllerTest_whenTextFieldIsEmpty_convertButtonIsNotEnabled() {
+    
+    // When
+    sut.textField.text = ""
+    sut.textField.sendActions(for: .editingChanged)
+    
+    // Then
+    XCTAssertFalse(sut.convertButton.isEnabled, "Convert button should be disabled")
+  }
+  
+  func testCurrencyConverterViewControllerTest_whenTextFieldHasValidData_convertButtonIsEnabled() {
+    
+    // When
+    sut.textField.text = "22"
+    sut.textField.sendActions(for: .editingChanged)
+    
+    // Then
+    XCTAssertTrue(sut.convertButton.isEnabled, "Convert button should be enabled")
+  }
+  
+  func testCurrencyConverterViewControllerTest_whenConvertButtonPressed_progressViewIsShown() {
+    
+    // Given
+    sut.textField.text = "22"
+
+    // When
+    sut.convertButton.sendActions(for: .touchUpInside)
+    RunLoop.current.run(until: Date())
+    let progressView = AppDelegate.shared.window?.subviews.first(where: { $0 as? InProgressView != nil })
+    
+    // Then
+    XCTAssertNotNil(progressView, "Progress view isn't shown")
+  }
 }
   
