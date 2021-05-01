d28a1161a8be861db0fe12a0ab8523e3c7a5a996
Amr Hesham <amrhesham9595@gmail.com>
2021-05-01 12:30:39 +0200

    - Add unit test to the project

---
+++ CCCalculator/CCCalculatorTests/ViewRelated/Controllers/CurrencyConverterViewController/CurrencyConverterViewControllerTests.swift
@@ -0,0 +1,38 @@
+//
+//  CurrencyConverterViewControllerTests.swift
+//  CCCalculatorTests
+//
+//  Created by Amr Hesham on 01/05/2021.
+//
+
+import XCTest
+@testable import CCCalculator
+
+// MARK: - CurrencyConverterViewControllerTests
+//
+class CurrencyConverterViewControllerTests: XCTestCase {
+  
+  // MARK: - Properties
+  
+  var sut: CurrencyConverterViewController!
+  
+  // MARK: - LifeCycle
+  
+  override func setUp() {
+    super.setUp()
+    
+//    sut = CurrencyConverterViewController(viewModel: <#T##CurrencyConverterViewModel#>)
+    
+    // load view hierarchy
+//    _ = sut.view
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+    sut = nil
+  }
+  
+  // MARK: - Tests
+  
+}
+  
